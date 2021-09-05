02fc9ed7ebbee21bb88d76690dc6883987d6a1b3
ritiksoni00 <rtksoni00@gmail.com>
2021-09-05 09:41:31 +0530

    set-env config var

--- jsonapi/settings.py
+++ jsonapi/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import environ
 
@@ -25,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = os.environ["DEBUG"]
 
 ALLOWED_HOSTS = []
 
