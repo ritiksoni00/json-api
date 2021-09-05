9676e13bd8e54fc4513da025da9b7c1b672df3d7
ritiksoni00 <rtksoni00@gmail.com>
2021-09-05 11:12:09 +0530

    used postgres

--- jsonapi/settings.py
+++ jsonapi/settings.py
@@ -87,13 +87,24 @@ WSGI_APPLICATION = "jsonapi.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+# i will update this when i know how to use env var
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'xepgspdy',
+        'USER': 'xepgspdy',
+        'PASSWORD': 'cy0KCyqPQn83jc6dJc5YGrfvHS6Yq4j4',
+        'HOST': 'rosie.db.elephantsql.com',
+        
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
